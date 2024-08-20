e290e3df1f0a10f081a2f97e2f0d5adf5fee02dc
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-20 16:03:34 +0200

    exemple server socket.io + react

---
+++ react/server/server.js
@@ -0,0 +1,13 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+    cors: {
+      origin: "http://localhost:3000"
+    }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connection");
+});
+  
+  io.listen(4000);
