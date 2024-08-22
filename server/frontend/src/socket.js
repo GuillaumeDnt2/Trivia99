d28b0da5d238acc805c25dccce9da7b610caa83f
vbonzon <113915093+vbonzon@users.noreply.github.com>
2024-08-22 13:43:02 +0200

    edit frontend

--- server/frontend/src/socket.js
+++ server/frontend/src/socket.js
@@ -1,8 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://backend:4000';
+const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
 
-export const socket = io("http://localhost:4000", {
+export const socket = io(URL, {
     autoConnect: false
   });
