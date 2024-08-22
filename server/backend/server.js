5dee2bd93ae935657385358716f150e8bed56426
vbonzon <113915093+vbonzon@users.noreply.github.com>
2024-08-22 11:18:08 +0200

    test dockerfile

--- server/backend/server.js
+++ server/backend/server.js
@@ -1,4 +1,29 @@
-import { Server } from "socket.io";
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000"
+  }
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
+
+server.listen(4000, () => {
+  console.log('listening on :4000');
+});
+
+
+/*import { Server } from "socket.io";
 
 const io = new Server({
     cors: {
@@ -10,6 +35,4 @@ io.on("connection", (socket) => {
   console.log("Connection");
 });
   
-  io.listen(4000);
-
-  ///
+io.listen(4000);*/
