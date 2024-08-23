3eb89ae8fd57095c5a31a3fb8463320094c4c622
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-23 10:19:12 +0200

    inversemment check local

--- server/frontend/src/socket.js
+++ server/frontend/src/socket.js
@@ -1,9 +1,9 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.VM_MOD === 'production' ? undefined : 'http://localhost:4000';
+const URL = process.env.LOCAL === 'true' ? 'http://localhost:4000' : undefined;
 console.log(URL);
-console.log(process.env.VM_MOD);
+console.log(process.env.LOCAL);
 export const socket = io(URL, {
     autoConnect: false
   });
