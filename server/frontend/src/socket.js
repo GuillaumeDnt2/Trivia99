2acffa1e9e90c0d7687e010ce36f83584c28ed7e
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-23 10:08:26 +0200

    changement nom variable environnement

--- server/frontend/src/socket.js
+++ server/frontend/src/socket.js
@@ -1,9 +1,9 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
+const URL = process.env.VM_MOD === 'production' ? undefined : 'http://localhost:4000';
 console.log(URL);
-console.log(process.env.NODE_ENV);
+console.log(process.env.VM_MOD);
 export const socket = io(URL, {
     autoConnect: false
   });
