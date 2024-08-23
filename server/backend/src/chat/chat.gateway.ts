409f2da677a2f3b34fbc5eed853379359a538314
neroil <edwin.haffner@heig-vd.ch>
2024-08-23 11:18:40 +0200

    added nestjs files

---
+++ server/backend/src/chat/chat.gateway.ts
@@ -0,0 +1,29 @@
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server } from 'socket.io';
+import { OnModuleInit } from '@nestjs/common';
+@WebSocketGateway()
+export class ChatGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('Connected');
+    });
+  }
+  @SubscribeMessage('message')
+  onNewMessage(@MessageBody() body: any) {
+    console.log(body);
+    this.server.emit('onMessage', {
+      msg: 'New Message',
+      content: body,
+    });
+  }
+}
