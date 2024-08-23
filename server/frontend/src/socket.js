407d60e8eb00759ffc4a7daae96187b70d5679a1
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-23 09:59:18 +0200

    ajout log NODE_ENV

--- server/frontend/src/socket.js
+++ server/frontend/src/socket.js
@@ -3,6 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
 console.log(URL);
+console.log(process.env.NODE_ENV);
 export const socket = io(URL, {
     autoConnect: false
   });
