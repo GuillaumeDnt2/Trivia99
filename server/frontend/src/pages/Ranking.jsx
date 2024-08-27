e2161d931d7364e47cd48992b430caf4fb57867f
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-27 11:05:14 +0200

    react sans css

--- server/frontend/src/pages/Ranking.jsx
+++ server/frontend/src/pages/Ranking.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from "react"
+import { socket } from "../utils/socket";
+
 export default function Ranking(){
-    return <h1>Rankings</h1>
+
+    const [ranking, setRanking] = useState([]);
+
+    useEffect(() => {
+        function onRanking(sentRanking){
+            setRanking(sentRanking);
+        }
+
+        socket.on("ranking", onRanking);
+
+        return () => {
+            socket.off("ranking");
+        }
+    }, []);
+
+    return <div className="vertical">
+                <img src="../../Trivia_99.png" alt="Trivia 99 logo"/>
+                <div className="rounded-box">
+                    <h2>{ranking[0]?.name} won!</h2>
+                    <h3>Ranking: </h3>
+                    <div className="scroll-box">
+                        <ul>
+                            {ranking.map(player =>
+                                <li>{player.name} : {player.score} answered correctly</li>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+                <button onClick={() => socket.emit('test')}>Test</button>
+            </div>
 }
