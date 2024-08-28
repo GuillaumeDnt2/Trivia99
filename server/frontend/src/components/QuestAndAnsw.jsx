6a534d35e78f2cfe9074020fdd3f8c581a73694b
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-28 14:22:52 +0200

    Questions and answers

---
+++ server/frontend/src/components/QuestAndAnsw.jsx
@@ -0,0 +1,28 @@
+import '../styles/common.css'
+import '../styles/QuestAndAnsw.css'
+import { socket } from '../utils/socket'
+
+export default function QuestAndAnsw(props){
+
+    function sendAnswer(val){
+        socket.emit("answer", val);
+    }
+
+
+    return <div className='content-column-box'>
+            {props.isAlive ? (
+                <>
+                <h3>{props.q}</h3>
+                <div className='answers-grid orange-border'>
+                    {props.a.map((answer, key) =>
+                                <div className='answer-cell' key={'Answ'+key} 
+                                onClick={() => sendAnswer(key)}>{answer}</div>
+                    )}
+                </div>
+                </>
+            ) : (
+                <h3>You lost !</h3>
+            )
+        }        
+        </div>
+}
