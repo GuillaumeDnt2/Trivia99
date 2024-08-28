0ae3c42946bf10fd887ed8c05216285abac140c6
vbonzon <valent1.bonz@gmail.com>
2024-08-28 11:44:06 +0200

    add coments and getters

--- server/backend/src/trivia/questionToSend.ts
+++ server/backend/src/trivia/questionToSend.ts
@@ -1,14 +1,40 @@
 import { QuestionInQueue } from "./questionInQueue";
 import { Question } from "./question";
 export class QuestionToSend extends QuestionInQueue {
-  category: string;
-  answers: string[];
-  question: string;
+
+  private category: string;
+  private answers: string[];
+  private question: string;
 
   constructor(q: Question, attack: boolean) {
     super(q, attack);
-    this.category = q.category;
-    this.answers = q.answers;
-    this.question = q.question;
+    this.category = q.getCategory();
+    this.answers = q.getAnswers();
+    this.question = q.getQuestion();
+  }
+
+
+  /**
+   * Category getter
+   * @returns question's category
+   */
+  public getCategory() : string{
+    return this.category;
+  }
+
+  /**
+   * Answers getter
+   * @returns question's answers
+   */
+  public getAnswers() : string[]{
+    return this.answers;
+  }
+
+  /**
+   * Question getter
+   * @returns question's question
+   */
+  public getQuestion() : string{
+    return this.question;
   }
 }
