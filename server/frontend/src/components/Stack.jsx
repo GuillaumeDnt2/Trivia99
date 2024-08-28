f6f39ee7918856986eda67ad70f5696572628925
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-08-28 14:46:01 +0200

    refactor stack display

--- server/frontend/src/components/Stack.jsx
+++ server/frontend/src/components/Stack.jsx
@@ -2,17 +2,11 @@ import "../styles/Stack.css";
 import StackQuestion from "./StackQuestion";
 
 export default function Stack(props){
-
-    const squareQ = []
-
-    for(let i = 0; i < props.state.length; i++){
-        let currQ = props.state.at(i)
-        squareQ.push(<StackQuestion attack={currQ.attack} difficulty={currQ.difficulty} />)
-    }
-
     return (
-        <div className={'stack-box'}>
-            {squareQ}
+        <div className='stack-box orange-border'>
+            {props.state?.map((currQ, key) => 
+                <StackQuestion key={'stack'+key} attack={currQ.attack} difficulty={currQ.difficulty} />
+            )}
         </div>
     );
 }
