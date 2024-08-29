9990475fab1321b261a20798940587bcd34e2f95
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2024-08-29 16:02:26 +0200

    Start ranking and put padding in solo class

--- server/frontend/src/pages/Test.jsx
+++ server/frontend/src/pages/Test.jsx
@@ -1,10 +1,149 @@
+import { useEffect, useState } from "react"
+import { socket } from "../utils/socket";
 import BaseLayout from "../components/BaseLayout";
-import Stack from "../components/Stack";
 
-export default function Test(){
-    return(
-        <BaseLayout>
-        <Stack state={[{attack : false, difficulty : "easy"}, {attack : false, difficulty : "medium"}, {attack : false, difficulty : "hard"}, {attack : true, difficulty : "easy"}, {attack : true, difficulty : "medium"}]}></Stack>
-        </BaseLayout>
-        )
+export default function Ranking(){
+
+    const [ranking, setRanking] = useState([
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+        {
+            "name": "player1",
+            "score": 100
+        },
+        {
+            "name": "player2",
+            "score": 50
+        },
+    ]);
+
+    useEffect(() => {
+        function onRanking(sentRanking){
+            setRanking(sentRanking);
+        }
+
+        socket.on("ranking", onRanking);
+
+        return () => {
+            socket.off("ranking");
+        }
+    }, []);
+
+    return <BaseLayout>
+        <div className={"padding-20px"}>
+            <div className={"center-text"}>
+                <h2>{ranking[0]?.name} won!</h2>
+            </div>
+            <h3>Ranking: </h3>
+            <div className="scroll-box">
+                <ul>
+                    {ranking.map(player =>
+                        <li>{player.name} : {player.score} answered correctly</li>
+                    )}
+                </ul>
+            </div>
+        </div>
+    </BaseLayout>
 }
