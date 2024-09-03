bd6da6f090a269d9c950489ab7b8de3f26f4fe91
neroil <edwin.haffner@heig-vd.ch>
2024-09-03 15:59:11 +0200

    adding a comment to see if the test pass now

--- server/frontend/src/utils/socket.js
+++ server/frontend/src/utils/socket.js
@@ -8,7 +8,7 @@ function checkCookieExists(name) {
 
 // "undefined" means the URL will be computed from the `window.location` object
 //const URL = process.env.LOCAL === 'true' ? 'http://localhost:4000' : 'http://localhost:4000';
-const URL = 'http://trivia99.zapto.org:4000';
+const URL = 'http://trivia99.zapto.org:4000';  //Le super url, il est beau non ?
 console.log(URL);
 console.log(process.env.LOCAL);
 
