8cec52a229cda223a0a63ce645efab4a9434582e
neroil <edwin.haffner@heig-vd.ch>
2024-09-03 15:50:46 +0200

    changed url

--- server/frontend/src/utils/socket.js
+++ server/frontend/src/utils/socket.js
@@ -7,8 +7,8 @@ function checkCookieExists(name) {
 }
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.LOCAL === 'true' ? 'http://localhost:4000' : 'http://localhost:4000';
-//const URL = 'http://trivia99.zapto.org:4000';
+//const URL = process.env.LOCAL === 'true' ? 'http://localhost:4000' : 'http://localhost:4000';
+const URL = 'http://trivia99.zapto.org:4000';
 console.log(URL);
 console.log(process.env.LOCAL);
 
