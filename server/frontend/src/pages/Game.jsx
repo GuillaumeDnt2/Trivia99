031ba1035037569887b51e701f5947b6f850847a
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-09-03 14:07:40 +0200

    Fix question parsing

--- server/frontend/src/pages/Game.jsx
+++ server/frontend/src/pages/Game.jsx
@@ -24,7 +24,9 @@ export default function Game(){
     const [rank, setRank] = useState(0);
 
     const [question, setQuestion] = useState("");
-    const [answers, setAnswers] = useState([])
+    const [answers, setAnswers] = useState([]);
+    const [attak, setAttack] = useState(false);
+    const [difficulty, setDifficulty] = useState("easy");
 
     const navigate = useNavigate();
 
@@ -50,8 +52,9 @@ export default function Game(){
         function onNewQuestion(question){
             setQuestion(question.question.text);
             setAnswers(question.answers);
-            console.log("Question received: " )
-            console.log(question.question.text)
+            setAttack(question.isAttack);
+            setDifficulty(question.difficulty);
+            console.log(question);
         }
 
         function onElimination(player){
@@ -111,7 +114,7 @@ export default function Game(){
                 <div className="content-column-box col2 game-container">
                     <Queue state={queue} />
                     <Stats streak={streak} accuracy={accuracy} nbReponse={nbResponse}/>
-                    <QuestAndAnsw isAlive={isAlive} q={question.text} a={question.answers} atk={question.isAttack} difficulty={question.difficulty} rank={rank}/>
+                    <QuestAndAnsw isAlive={isAlive} q={question} a={answers} atk={question.isAttack} difficulty={question.difficulty} rank={rank}/>
                     <AttackBtn canAttack={canAttack}/>
                 </div>
                 <PlayerList col="col3" players={playersRight}/>
