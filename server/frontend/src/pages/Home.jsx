ef0d4ae6bf94842ffd0d42fe0822f86930f28de9
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-09-04 10:14:27 +0200

    Change about into rules and and add link to landing page

--- server/frontend/src/pages/Home.jsx
+++ server/frontend/src/pages/Home.jsx
@@ -87,97 +87,13 @@ export default function Home(){
                     </label>
                     <button type="submit" className={"orange-button"} disabled={gameStarted}>{gameStarted ? "Game in progress..." : "Click to play !"}</button>
                 </form>
-                <button onClick={() => navigate("/about") } className={"content-row-box center gap-in-button orange-button"}>About the game</button>
-                <Link to="https://github.com/GuillaumeDnt2/Trivia99/tree/main">
+                <button onClick={() => navigate("/about") } className={"content-row-box center gap-in-button orange-button"}>Rules</button>
+                <Link to="https://guillaumednt2.github.io/Trivia99/" className="no-underline" target="_blank">
+                    <button className={"content-row-box center gap-in-button orange-button"}>About</button>
+                </Link>
+                <Link to="https://github.com/GuillaumeDnt2/Trivia99/tree/main" className="no-underline" target="_blank">
                     <button type="button" className={"content-row-box center gap-in-button orange-button"}>GitHub<img src={github} alt="GitHub" className={"image-git"}/></button>
                 </Link>
             </div>
         </BaseLayout>
 }
-
-
-
-/*
-import "../styles/Home.css";
-import { socket } from "../utils/socket.js";
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import github from "../assets/github-mark.svg";
-import BaseLayout from "../components/BaseLayout";
-
-export default function Home() {
-    const [name, setName] = useState("");
-    const [placeholder, setPlaceholder] = useState("Enter your name");
-    const [gameStarted, setGameStarted] = useState(false);
-
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        const handleStartGame = () => setGameStarted(true);
-        const handleRanking = () => setGameStarted(false);
-        const handleGameStatus = (state) => {
-            if (state === "started") {
-                setGameStarted(true);
-
-            } else {
-                setGameStarted(false);
-            }
-        };
-        const handleLoggedInfo = () => navigate("/waiting");
-
-        socket.on("startGame", handleStartGame);
-        socket.on("ranking", handleRanking);
-        socket.on("gameStatus", handleGameStatus);
-        socket.on("loggedInfo", handleLoggedInfo);
-
-        socket.emit("getGameStatus");
-
-        return () => {
-            socket.off("startGame", handleStartGame);
-            socket.off("ranking", handleRanking);
-            socket.off("gameStatus", handleGameStatus);
-            socket.off("loggedInfo", handleLoggedInfo);
-        };
-    }, [navigate]);
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (name.length !== 0) {
-            socket.emit("login", name);
-        } else {
-            document.querySelector("input").style.setProperty("--c", "red");
-            setPlaceholder("You must enter a name...");
-        }
-    };
-
-    return (
-        <BaseLayout>
-            <div className={"content-column-box center padding-20px"}>
-                <form onSubmit={handleSubmit} className="content-column-box">
-                    <label className={"content-row-box"}>
-                        <p className={"username-home"}>Username</p>
-                        <input
-                            type="text"
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder={placeholder}
-                        />
-                    </label>
-                    <button type="submit" className={"orange-button"}>
-                        Click to play !
-                    </button>
-                </form>
-                <Link to="https://github.com/GuillaumeDnt2/Trivia99/tree/main">
-                    <button
-                        type="button"
-                        className={"content-row-box center gap-in-button orange-button"}
-                    >
-                        GitHub
-                        <img src={github} alt="GitHub" className={"image-git"} />
-                    </button>
-                </Link>
-            </div>
-        </BaseLayout>
-    );
-}
-
- */
