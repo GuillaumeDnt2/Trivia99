027b9dffb444c0151ef878ce7bba53f66546d508
Neroil <edwin.haffner@heig-vd.ch>
2024-09-04 16:39:18 +0200

    corrected english kinda

--- server/frontend/src/pages/Waiting.jsx
+++ server/frontend/src/pages/Waiting.jsx
@@ -44,10 +44,10 @@ export default function Waiting(){
                 <p className={"text"}>{playersInfo.nbReady}/{playersInfo.nbPlayers} player(s) ready</p>
                 <button id="readyBtn" className={'orange-button'} onClick={onClick} disabled={ready}>Ready ?</button>
                 <div className="quick-rule">
-                    <h3>Quick rule</h3>
-                    <p>Your goal is to answer the questions in your queue. Questions arrive in your queue at a regular time span (but this one shrinks as the time pass). If your queue is full (<b>8</b> questions or more), you are eliminated.</p>
+                    <h3>Quick Rules.</h3>
+                    <p>Your goal is to answer the questions in your queue. Questions arrive in your queue at regular intervals, which shrink as time passes. If your queue is full (<b>8</b> questions or more), you are eliminated.</p>
                     <p>Three types of questions : <span style={{color: "var(--easy)"}}>easy</span>, <span style={{color: "var(--medium)"}}>medium</span>, <span style={{color: "var(--hard)"}}>hard</span>.</p>
-                    <p>You can chain the correct answer: it's your <b>streak</b>. Once your streak reach 3 or more, you can press the attack button. It will send an additional question to a random player (display in <span style={{color: "var(--attack)"}}>red</span> in their stack). The greater the streak, the more questions are sent.</p>
+                    <p>You can chain correct answers: it's your <b>streak</b>. Once your streak reaches 3 or more, you can press the attack button. It will send additional questions to random players (display in <span style={{color: "var(--attack)"}}>red</span> in their stack). The greater the streak, the more questions are sent.</p>
                 </div>
         </div>
         </BaseLayout>
